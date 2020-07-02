56ee0b9abdf2a1b9d52ae02c3493083d6baa83a5
Isidro Torregrosa <isistorre95@gmail.com>
2020-07-02 12:23:58 +0200

    login

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,10 +1,15 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup} from '@angular/forms';
 import { LoginService } from './login.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { Router } from '@angular/router';
+import { ErrorsService } from 'src/app/core/validation/errors.service';
 
 @Component({
   selector: 'ps-login',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   @ViewChild('form') form: FormGroup;
@@ -12,7 +17,38 @@ export class LoginComponent {
     email: '',
     password: ''
   };
-  constructor(private loginService: LoginService) {}
+  errors = {
+    email: {
+      required: 'Email is required',
+      email: 'Invalid email'
+    },
+    password: {
+      required: 'Password is required'
+    }
+  };
+  loginError = '';
+  constructor(
+    private loginService: LoginService,
+    private authService: AuthService,
+    private router: Router,
+    public errorsService: ErrorsService
+  ) { }
   onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+    this.loginService.login(this.model)
+      .then(this.onLoginSuccess)
+      .catch(this.onLoginError);
+  }
+  private onLoginSuccess = (data: {token: string}) => {
+    this.authService.token = data.token;
+    this.router.navigateByUrl('/', {replaceUrl: true});
+  }
+  private onLoginError = (resp: HttpErrorResponse) => {
+    const error = resp.status === 401
+      ? 'Incorrect credentials'
+      : 'Unexpected error';
+    this.loginError = error;
   }
 }
--- src/app/public/login/login.module.ts
+++ src/app/public/login/login.module.ts
@@ -3,6 +3,10 @@ import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login.routing';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,11 @@ import { LoginRoutingModule } from './login.routing';
   imports: [
     CommonModule,
     FormsModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class LoginModule { }

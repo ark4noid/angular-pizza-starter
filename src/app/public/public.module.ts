5a7493de4470f6730d650f2d343efe25fc1e2aa4
YolandaRm <yramosmartin@gmail.com>
2020-07-02 09:54:37 +0200

    primer

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PublicComponent } from './public.component';
 import { PublicRoutingModule } from './public.routing';
+import { PublicLayoutComponent } from './public-layout.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [PublicComponent],
-  imports: [PublicRoutingModule]
+  declarations: [
+    PublicComponent,
+    PublicLayoutComponent
+  ],
+
+  imports: [
+    PublicRoutingModule,
+    MatToolbarModule
+  ]
 })
 export class PublicModule {}

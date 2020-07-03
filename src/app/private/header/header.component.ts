4219acb46e3a202761525dd7723f50108e4c3300
ark4noid <ark4no07@gmail.com>
2020-07-03 09:12:10 +0200

    header

---
+++ src/app/private/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ps-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent {
+
+  }
+
+
+  // Lanzar un evento de nombre menuClick cuando se pinche en el icono de menu
+  // Estudiar @output en Angular (https://angular.io/guide/component-interaction#parent-listens-for-child-event)
+  // Estudiar los event bindings en Angular (https://angular.io/guide/template-syntax#event-binding-event)

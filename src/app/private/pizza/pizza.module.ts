6646c6aca94e2659f1d14cab7d3f13640002ad5f
ark4noid <ark4no07@gmail.com>
2020-07-09 13:59:15 +0200

    pizza-list

---
+++ src/app/private/pizza/pizza.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { PizzaRoutingModule } from './pizza.routing';
+
+@NgModule({
+    imports:[
+        PizzaRoutingModule,
+    ]
+})
+
+export class PizzaModule{
+
+}

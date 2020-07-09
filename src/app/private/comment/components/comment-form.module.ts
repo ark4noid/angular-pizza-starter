918116c7dc0bd105c9fc130e0cee55cb01608803
YolandaRm <yramosmartin@gmail.com>
2020-07-09 09:30:18 +0200

    comenzando el form

--- src/app/private/comment/components/comment-form.module.ts
+++ src/app/private/comment/components/comment-form.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommentFormComponent } from './comment-form.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
     declarations: [     //elemento del modulo
         CommentFormComponent,
     ],
-    imports:[],
+    imports:[
+        MatFormFieldModule,
+        MatInputModule
+    ],
     
     exports: [
         CommentFormComponent,   // para poder usarlo desde fuera del modulo
     ],
 })
-export class CommentFormModule {}
+export class CommentFormModule {
+}

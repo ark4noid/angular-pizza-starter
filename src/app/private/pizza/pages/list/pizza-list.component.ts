b6bbb30ff4effaefdb9eaa4e2a0e7a95c614def7
Isidro Torregrosa <isistorre95@gmail.com>
2020-07-10 13:36:00 +0200

    pizza card click

--- src/app/private/pizza/pages/list/pizza-list.component.ts
+++ src/app/private/pizza/pages/list/pizza-list.component.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaHttpService } from '../../services/pizza-http.service';
+import { Router } from '@angular/router';
 
 @Component({
-    selector:'ps-pizza-list',
-    templateUrl:'./pizza-list.component.html',
-    styleUrls:['./pizza-list.component.css'],
-    providers:[PizzaHttpService]
+  selector: 'ps-pizza-list',
+  templateUrl: './pizza-list.component.html',
+  styleUrls: ['./pizza-list.component.css'],
+  providers: [PizzaHttpService]
 })
-export class PizzaListComponent implements OnInit{
-    pizzas:any;
-    constructor(private pizzaService:PizzaHttpService){
-    }
-    ngOnInit(){
-        this.getPizzas();
-    }
-    getPizzas(){
-        this.pizzaService.getAll().then((pizzas)=>{
-            this.pizzas=pizzas;
-        })
-    }
-    getImg(pizza){
-        return `http://localhost:3005${pizza.img}`;
-
-    }
-   
+export class PizzaListComponent implements OnInit {
+  pizzas: any;
+  constructor(private pizzaService: PizzaHttpService, private router: Router) {
+  }
+  ngOnInit() {
+    this.getPizzas();
+  }
+  getPizzas() {
+    this.pizzaService.getAll().then((pizzas) => {
+      this.pizzas = pizzas;
+    })
+  }
+  getImg(pizza) {
+    return `http://localhost:3005${pizza.img}`;
+  }
+  onClick(pizza){
+    this.router.navigateByUrl(`/pizzas/${pizza.id}`);
+  }
 }
